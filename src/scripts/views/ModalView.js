b79ca3c48cb576edc565cb742f0bec06fa219a37
peter <peter.shenlq@gmail.com>
2016-05-09 18:54:26 +0800

    UPDATE: 更新modal demo。

--- src/scripts/views/ModalView.js
+++ src/scripts/views/ModalView.js
@@ -12,8 +12,8 @@ export default class ModalView extends Component{
                                 <div className="modal-dialog">
                                     <div className="modal-content">
                                         <div className="modal-header">
-                                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                                <span aria-hidden="true">&times;</span>
+                                            <button type="button" className="close">
+                                                &times;
                                             </button>
                                             <h5 className="modal-title">Modal title</h5>
                                         </div>
@@ -21,8 +21,8 @@ export default class ModalView extends Component{
                                             <p>One fine body&hellip;</p>
                                         </div>
                                         <div className="modal-footer">
-                                            <button type="button" className="btn btn-default btn-sm">Close</button>
-                                            <button type="button" className="btn btn-primary btn-sm">Save</button>
+                                            <button type="button" className="btn btn-default">Close</button>
+                                            <button type="button" className="btn btn-primary">Save</button>
                                         </div>
                                     </div>
                                 </div>
