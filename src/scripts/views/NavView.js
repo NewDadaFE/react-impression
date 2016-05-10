02d6229c8833342eec41986962de6071c65f9f1b
peter <peter.shenlq@gmail.com>
2016-05-10 11:44:29 +0800

    [ADD]: 添加nav、nav-tabs、nav-pills组件样式。

--- src/scripts/views/NavView.js
+++ src/scripts/views/NavView.js
@@ -4,13 +4,76 @@ export default class NavView extends Component{
     render(){
         return (
             <div>
-                <h3>Examples</h3>
+                <h3>Inline</h3>
                 <div className="card card-block">
                     <div className="row">
-                        <div className="col-sm-6">
-                            <ul className="nav">
+                        <div className="col-sm-12">
+                            <ul className="nav nav-inline">
                                 <li className="nav-item">
                                     <a className="nav-link" href="#">Link</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Link</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Another link</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link disabled" href="#">Disabled</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <h3>Tabs</h3>
+                <div className="card card-block">
+                    <div className="row">
+                        <div className="col-sm-12">
+                            <ul className="nav nav-tabs">
+                                <li className="nav-item">
+                                    <a className="nav-link active" href="#">Active</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Link</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Another link</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link disabled" href="#">Disabled</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <h3>Pills</h3>
+                <div className="card card-block">
+                    <div className="row">
+                        <div className="col-sm-12">
+                            <ul className="nav nav-pills">
+                                <li className="nav-item">
+                                    <a className="nav-link active" href="#">Active</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Link</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Another link</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link disabled" href="#">Disabled</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <h3>Stacked pills</h3>
+                <div className="card card-block">
+                    <div className="row">
+                        <div className="col-sm-2">
+                            <ul className="nav nav-pills nav-stacked">
+                                <li className="nav-item">
+                                    <a className="nav-link active" href="#">Active</a>
                                 </li>
                                 <li className="nav-item">
                                     <a className="nav-link" href="#">Link</a>
