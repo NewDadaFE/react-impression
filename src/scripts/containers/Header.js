8e98e48d541fcada527876f61a35fa7be4ab05be
peter <peter.shenlq@gmail.com>
2016-05-11 21:25:08 +0800

    [UPDATE]: 先这样吧，布局明天优先调调。

--- src/scripts/containers/Header.js
+++ src/scripts/containers/Header.js
@@ -10,26 +10,20 @@ export default class Header extends Component{
             <div>
                 <nav className="navbar">
                     <a className="navbar-brand" href="#">Navbar</a>
-                    <ul className="nav navbar-nav">
+                    <ul className="nav navbar-nav pull-xs-right">
                         <li className="nav-item active">
-                            <a className="nav-link" href="#">Home</a>
+                            <a className="nav-link" href="#">首页</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Features</a>
+                            <a className="nav-link" href="#">PC端</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Pricing</a>
+                            <a className="nav-link" href="#">移动端</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">About</a>
+                            <a className="nav-link" href="#">动画</a>
                         </li>
                     </ul>
-                    <form className="form form-inline pull-xs-right">
-                        <div className="form-group">
-                            <input className="form-control" type="text" placeholder="Search"/>
-                        </div>
-                        <button className="btn btn-primary" type="submit">Search</button>
-                    </form>
                 </nav>
             </div>
         );
