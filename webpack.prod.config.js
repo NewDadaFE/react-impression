4cd84e3db5271a95670cc4a4786692109ab864d7
peter <peter.shenlq@gmail.com>
2016-05-19 09:55:20 +0800

    [UPDATE]: 修改web pack配置。

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -2,9 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    entry: [
-        './src/scripts/index'
-    ],
+    entry: './src/scripts/index',
     output: {
         path: path.join(__dirname, 'build', 'scripts'),
         filename: 'app.js',
