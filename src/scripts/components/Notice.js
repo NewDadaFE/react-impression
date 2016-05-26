ee48d0aa3d76a278161934d8af38c87f09668532
peter <peter.shenlq@gmail.com>
2016-05-26 11:51:39 +0800

    [UPDATE]:  调整Notice样式并从Notification中拆分出来。

---
+++ src/scripts/components/Notice.js
@@ -0,0 +1,65 @@
+import classnames from 'classnames';
+import React, { Component, PropTypes } from 'react';
+
+/**
+ * Notice组件.
+ */
+export default class Notice extends Component{
+    //prop type校验
+    static propTypes = {
+        //类型
+        style: PropTypes.oneOf(['info', 'success', 'warning', 'danger']),
+        //标题
+        title: PropTypes.string,
+        //内容
+        message: PropTypes.string,
+        //关闭
+        close: PropTypes.func,
+    }
+    //默认props
+    static defaultProps = {
+        style: 'info',
+        title: '通知',
+    }
+    /**
+     * 获取信息图标.
+     * @return {[type]} [description]
+     */
+    getTitleIcon(){
+        let { style } = this.props;
+
+        return {
+            info:     ['fa', 'fa-volume-up'],
+            success:  ['fa', 'fa-check-circle'],
+            warning:  ['fa', 'fa-exclamation-triangle'],
+            danger:   ['fa', 'fa-times-circle'],
+        }[style];
+    }
+    /**
+     * 渲染.
+     */
+    render() {
+        let { title, message, style, closeable, close, children, className } = this.props;
+        let styleClass = `notice-${style}`;
+        let iconClass = this.getTitleIcon();
+
+        return (
+            <div className={classnames('notice', styleClass, className)}>
+                <div className="notice-header">
+                    <i className={classnames(iconClass)}></i>
+                </div>
+                <div className="notice-body">
+                    <div className="notice-title">
+                        {title}
+                        { closeable &&
+                            <button type="button" className="close" onClick={close}>&times;</button>
+                        }
+                    </div>
+                    <div className="notice-content">
+                        {message || children}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
