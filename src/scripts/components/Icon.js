66b9ec3e8f3560a29919fa17bc1e76b0e8464524
peter <peter.shenlq@gmail.com>
2016-05-26 11:40:36 +0800

    [UPDATE]: 给Icon添加size和left、right属性。

--- src/scripts/components/Icon.js
+++ src/scripts/components/Icon.js
@@ -7,15 +7,32 @@ import React, { Component, PropTypes } from 'react';
 export default class Icon extends Component{
     //prop type校验
     static propTypes = {
+        //图标类型
         type: PropTypes.string,
+        //是否在左边
+        left: PropTypes.bool,
+        //是否在右边
+        right: PropTypes.bool,
+        //尺寸
+        size: PropTypes.string,
+        //自定义样式
+        className: PropTypes.string,
+    }
+    //默认props
+    static defaultProps = {
+        left: false,
+        right: false,
     }
     //渲染
     render(){
-        let { type, className, ...others } = this.props,
-            typeClass = `fa-${type}`;
+        let { type, size, left, right, className, ...others } = this.props,
+            typeClass = `fa-${type}`,
+            sizeClass = size? `fa-${size}` : null,
+            leftClass = left? 'fa-left' : null,
+            rightClass = right? 'fa-right' : null;
 
         return(
-           <i {...others} className={classnames('fa', typeClass, className)}></i>
+           <i {...others} className={classnames('fa', typeClass, sizeClass, leftClass, rightClass, className)}></i>
         );
     }
 }
--- src/scripts/site/containers/Sidebar.js
+++ src/scripts/site/containers/Sidebar.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { Icon } from '../../components';
+
 
 /**
  * 侧边栏Sidebar.
@@ -15,26 +17,26 @@ export default class Sidebar extends Component {
                     <div className="nav-item">
                         BASIC
                     </div>
-                    <Link className="nav-link" to="/button"><i className="fa fa-hand-pointer-o fa-left"></i>Button</Link>
-                    <Link className="nav-link" to="/table"><i className="fa fa-table fa-left"></i>Table</Link>
-                    <Link className="nav-link" to="/form"><i className="fa fa-file-text-o fa-left"></i>Form<span className="fa-right tag tag-danger">Hot</span></Link>
-                    <Link className="nav-link" to="/image"><i className="fa fa-picture-o fa-left"></i>Image</Link>
+                    <Link className="nav-link" to="/button"><Icon type="hand-pointer-o" left/>Button</Link>
+                    <Link className="nav-link" to="/table"><Icon type="table" left/>Table</Link>
+                    <Link className="nav-link" to="/form"><Icon type="file-text-o" left/>Form<span className="fa-right tag tag-danger">Hot</span></Link>
+                    <Link className="nav-link" to="/image"><Icon type="picture-o" left/>Image</Link>
                     <div className="nav-item">
                         COMPONENT
                     </div>
-                    <Link className="nav-link" to="/card"><i className="fa fa-television fa-left"></i>Card</Link>
-                    <Link className="nav-link" to="/breadcrumb"><i className="fa fa-hand-o-right fa-left"></i>Breadcrumb</Link>
-                    <Link className="nav-link" to="/tag"><i className="fa fa-tag fa-left"></i>Tag</Link>
-                    <Link className="nav-link" to="/alert"><i className="fa fa-info-circle fa-left"></i>Alert<span className="fa-right tag tag-warning">1/4</span></Link>
-                    <Link className="nav-link" to="/progress"><i className="fa fa-hourglass-start fa-left"></i>Progress</Link>
-                    <Link className="nav-link" to="/button-group"><i className="fa fa-th-large fa-left"></i>ButtonGroup</Link>
-                    <Link className="nav-link" to="/pagination"><i className="fa fa-angle-double-right fa-left"></i>Pagination</Link>
-                    <Link className="nav-link" to="/nav"><i className="fa fa-bars fa-left"></i>Nav</Link>
-                    <Link className="nav-link" to="/navbar"><i className="fa fa-tasks fa-left"></i>Navbar</Link>
-                    <Link className="nav-link" to="/input-group"><i className="fa fa-outdent fa-left"></i>InputGroup</Link>
-                    <Link className="nav-link" to="/list-group"><i className="fa fa-list fa-left"></i>ListGroup</Link>
+                    <Link className="nav-link" to="/card"><Icon type="television" left/>Card</Link>
+                    <Link className="nav-link" to="/breadcrumb"><Icon type="hand-o-right" left/>Breadcrumb</Link>
+                    <Link className="nav-link" to="/tag"><Icon type="tag" left/>Tag</Link>
+                    <Link className="nav-link" to="/alert"><Icon type="info-circle" left/>Alert<span className="fa-right tag tag-warning">1/4</span></Link>
+                    <Link className="nav-link" to="/progress"><Icon type="hourglass-start" left/>Progress</Link>
+                    <Link className="nav-link" to="/button-group"><Icon type="th-large" left/>ButtonGroup</Link>
+                    <Link className="nav-link" to="/pagination"><Icon type="angle-double-right" left/>Pagination</Link>
+                    <Link className="nav-link" to="/nav"><Icon type="bars" left/>Nav</Link>
+                    <Link className="nav-link" to="/navbar"><Icon type="tasks" left/>Navbar</Link>
+                    <Link className="nav-link" to="/input-group"><Icon type="outdent" left/>InputGroup</Link>
+                    <Link className="nav-link" to="/list-group"><Icon type="list" left/>ListGroup</Link>
                     <div className="nav-collapse">
-                        <div className="nav-link"><i className="fa fa-list fa-left"></i>List-Group<i className="fa fa-angle-right pull-right"></i></div>
+                        <div className="nav-link"><Icon type="list" left/>List-Group<Icon type="angle-right" className="pull-right"/></div>
                         <div className="nav-collapse-block">
                             <Link className="nav-link" to="/image">Image</Link>
                             <Link className="nav-link" to="/table">Table</Link>
@@ -42,21 +44,21 @@ export default class Sidebar extends Component {
                             <Link className="nav-link" to="/button-group">Button-Group</Link>
                         </div>
                     </div>
-                    <Link className="nav-link" to="/modal"><i className="fa fa-file-text fa-left"></i>Modal<span className="fa-right tag tag-success">New</span></Link>
-                    <Link className="nav-link" to="/tooltip"><i className="fa fa-commenting-o fa-left"></i>Tooltip</Link>
-                    <Link className="nav-link" to="/popover"><i className="fa fa-comments fa-left"></i>Popover</Link>
-                    <Link className="nav-link" to="/dropdown"><i className="fa fa-caret-square-o-down fa-left"></i>Dropdown</Link>
-                    <Link className="nav-link" to="/message"><i className="fa fa-bell fa-left"></i>Message</Link>
-                    <Link className="nav-link" to="/notification"><i className="fa fa-bullhorn fa-left"></i>Notification</Link>
-                    <Link className="nav-link" to="/date-picker"><i className="fa fa-calendar fa-left"></i>DatePicker</Link>
-                    <Link className="nav-link" to="/timeline"><i className="fa fa-clock-o fa-left"></i>Timeline</Link>
+                    <Link className="nav-link" to="/modal"><Icon type="file-text" left/>Modal<span className="fa-right tag tag-success">New</span></Link>
+                    <Link className="nav-link" to="/tooltip"><Icon type="commenting-o" left/>Tooltip</Link>
+                    <Link className="nav-link" to="/popover"><Icon type="comments" left/>Popover</Link>
+                    <Link className="nav-link" to="/dropdown"><Icon type="caret-square-o-down" left/>Dropdown</Link>
+                    <Link className="nav-link" to="/message"><Icon type="bell" left/>Message</Link>
+                    <Link className="nav-link" to="/notification"><Icon type="bullhorn" left/>Notification</Link>
+                    <Link className="nav-link" to="/date-picker"><Icon type="calendar" left/>DatePicker</Link>
+                    <Link className="nav-link" to="/timeline"><Icon type="clock-o" left/>Timeline</Link>
                 </div>
                 <div className="sidebar-footer">
                     <a href="javascript:void(0)" className="sidebar-footer-item">
-                        <i className="fa fa-lg fa-question-circle fa-left"></i>帮助
+                        <Icon type="question-circle" size="lg" left/>帮助
                     </a>
                     <a href="javascript:void(0)" className="sidebar-footer-item">
-                        <i className="fa fa-power-off fa-lg fa-left"></i>登出
+                        <Icon type="power-off" size="lg" left/>登出
                     </a>
                 </div>
             </div>
