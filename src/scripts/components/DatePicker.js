fb148b6d49c9accadfdf7210f716930d78281c12
peter <peter.shenlq@gmail.com>
2016-05-27 19:24:07 +0800

    [UPDATE]: 添加年份无限生成、最大最小期限。

--- src/scripts/components/DatePicker.js
+++ src/scripts/components/DatePicker.js
@@ -16,15 +16,21 @@ export default class DatePicker extends Component{
     //构造函数
     constructor(props, context){
         super(props, context);
-        let { firstDayOfWeek, weekdays } = this.sortWeekdays();
+        let { date, minDate, maxDate, format } = props,
+            { firstDayOfWeek, weekdays } = this.sortWeekdays(),
+            currentMoment = date? moment(date, format) : moment(),
+            years = this.getYears(currentMoment);
+
         this.state = {
-            currentMoment: moment(),//当前时间
+            currentMoment,//当前时间
             days: [],//日期选择
             checkedDay: undefined,//选中日期
             weekdays,//星期
             firstDayOfWeek,//星期第一天
-            panel: 'day',
-            years: []
+            panel: 'day',//面板
+            years,//年份
+            minDate: minDate? moment(minDate, format) : undefined,//最小日期
+            maxDate: maxDate? moment(maxDate, format) : undefined,//最大日期
         };
 
         this.prevMonthHandle = this.prevMonthHandle.bind(this);
@@ -36,11 +42,16 @@ export default class DatePicker extends Component{
         this.selectYearHandle = this.selectYearHandle.bind(this);
         this.selectMonthHandle = this.selectMonthHandle.bind(this);
         this.showMonthPanelHandle = this.showMonthPanelHandle.bind(this);
+        this.scrollYearHandle = this.scrollYearHandle.bind(this);
     }
     //prop type校验
     static propTypes = {
         //自定义样式
         className: PropTypes.string,
+        //日期
+        date: PropTypes.string,
+        //格式
+        format: PropTypes.string,
         //星期
         weekdays: PropTypes.arrayOf(PropTypes.string),
         //月份
@@ -51,14 +62,24 @@ export default class DatePicker extends Component{
         todayText: PropTypes.string,
         //星期第一天
         firstDayOfWeek: PropTypes.string,
+        //最小日期
+        minDate: PropTypes.string,
+        //最大日期
+        maxDate: PropTypes.string,
+        //年份前后默认范围
+        yearScope: PropTypes.oneOfType([
+            PropTypes.string,
+            PropTypes.number,
+        ]),
     }
     //默认props
     static defaultProps = {
+        format: FORMAT.DATE,
         weekdays: ['日', '一', '二', '三', '四', '五', '六'],
         showToday: true,
         todayText: '今天',
         firstDayOfWeek: '日',
-        years: [2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017],
+        yearScope: 5,
         months: ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月'],
     }
     /**
@@ -81,7 +102,7 @@ export default class DatePicker extends Component{
      * @param  {[Moment]} currentMoment [当前日期]
      */
     getDate(currentMoment){
-        let { firstDayOfWeek } = this.state,
+        let { firstDayOfWeek, minDate, maxDate } = this.state,
             days = [],
             today = moment().format(FORMAT.DATE),
             prevMonth = moment(currentMoment).subtract(1,'months'),
@@ -101,6 +122,7 @@ export default class DatePicker extends Component{
                 date: dayMoment,
                 inMonth: true,
                 isToday: dayMoment.isSame(today),
+                disable: minDate && dayMoment.isBefore(minDate) || maxDate && dayMoment.isAfter(maxDate),
             });
         }
         //上个月天数
@@ -118,6 +140,7 @@ export default class DatePicker extends Component{
                 text: prevMonthDaysLength - i,
                 date: dayMoment,
                 isToday: dayMoment.isSame(today),
+                disable: minDate && dayMoment.isBefore(minDate) || maxDate && dayMoment.isAfter(maxDate),
             });
         }
         //下个月天数
@@ -134,6 +157,7 @@ export default class DatePicker extends Component{
                 text: i,
                 date: dayMoment,
                 isToday: dayMoment.isSame(today),
+                disable: minDate && dayMoment.isBefore(minDate) || maxDate && dayMoment.isAfter(maxDate),
             });
         }
 
@@ -141,6 +165,22 @@ export default class DatePicker extends Component{
             days,
             currentMoment,
         });
+    }
+    /**
+     * 根据当前时间获取年份列表.
+     * @param  {[Moment]} currentMoment [当前时间]
+     * @return {[Array]}                [年份列表]
+     */
+    getYears(currentMoment){
+        let { yearScope } = this.props,
+            years = [],
+            currentYear = currentMoment.year();
+
+        for(let i = currentYear - yearScope; i <= currentYear + yearScope; i++){
+            years.push(i);
+        }
+
+        return years;
     }
     /**
      * 上个月.
@@ -183,22 +223,29 @@ export default class DatePicker extends Component{
      * @param  {[String]} year [年份]
      */
     selectYearHandle(year){
-        let { currentMoment } = this.state;
+        let { currentMoment } = this.state,
+            newMoment = moment(currentMoment).year(year);
 
         this.setState({
-            currentMoment:  moment(currentMoment).year(year)
+            checkedDay: undefined,
+            currentMoment:  newMoment
         });
+        this.getDate(newMoment);
     }
     /**
      * 选中月份.
      * @param  {[String]} month [月份]
      */
     selectMonthHandle(month){
-        let { currentMoment } = this.state;
+        let { currentMoment } = this.state,
+            newMoment = moment(currentMoment).month(month);
+
         this.setState({
             panel: 'day',
-            currentMoment: moment(currentMoment).month(month)
+            checkedDay: undefined,
+            currentMoment: newMoment
         });
+        this.getDate(newMoment);
     }
     /**
      * 今天.
@@ -219,14 +266,59 @@ export default class DatePicker extends Component{
             panel: panel === 'month'? 'day' : 'month'
         });
     }
+    componentDidUpdate(){
+        let { _yeargroup, _monthgroup } = this.refs;
+        if(!this._activeYear || !this._activeMonth || _yeargroup.scrollTop !== 0 || _monthgroup.scrollTop !== 0 ){
+            return false;
+        }
+
+        let yearHeight = this._activeYear.getBoundingClientRect().height,
+            monthHeight = this._activeMonth.getBoundingClientRect().height;
+
+        this._yeargroupScrollTop = _yeargroup.scrollTop = this._activeYear.offsetTop - _yeargroup.offsetTop - yearHeight * 2;
+        _monthgroup.scrollTop = this._activeMonth.offsetTop - _monthgroup.offsetTop - monthHeight * 2;
+    }
     componentDidMount(){
         let { currentMoment } = this.state;
         this.getDate(currentMoment);
     }
+    /**
+     * 无限获取年份.
+     * @param  {[Event]} event [事件]
+     */
+    scrollYearHandle(event){
+        let { years } = this.state,
+            { yearScope } = this.props,
+            container = event.target,
+            yearHeight = this._activeYear.getBoundingClientRect().height,
+            point = container.getBoundingClientRect().height + yearHeight;
+
+        //上滑
+        if(this._yeargroupScrollTop > container.scrollTop){
+            this._yeargroupScrollTop = container.scrollTop;
+            if(container.scrollTop < yearHeight){
+                let prevYear = years[0] - 1;
+                this.setState({
+                    years: [prevYear, ...years]
+                });
+                container.scrollTop = container.scrollTop === 0? yearHeight : (container.scrollTop + yearHeight);
+            }
+        }else if(this._yeargroupScrollTop < container.scrollTop){
+            //下滑
+            this._yeargroupScrollTop = container.scrollTop;
+            if(container.scrollHeight - container.scrollTop < point){
+                let nextYear = years[years.length - 1] + 1;
+                this.setState({
+                    years: [...years, nextYear]
+                });
+            }
+        }
+
+    }
     //渲染
     render(){
-        let { panel, currentMoment, days, checkedDay, weekdays } = this.state,
-            { showToday, todayText, years, months, className, ...others } = this.props,
+        let { panel, currentMoment, days, checkedDay, weekdays, years } = this.state,
+            { showToday, todayText, months, className, ...others } = this.props,
             currentYear = currentMoment.format(FORMAT.YEAR),
             currentMonth = currentMoment.format(FORMAT.MONTH);
 
@@ -250,13 +342,14 @@ export default class DatePicker extends Component{
                             )}
                         </div>
                         <div className="datepicker-daygroup">
-                            { days.map(({ text, date, checked, isToday, inMonth}, index) =>
-                                <div key={index} onClick={() => this.selectDateHandle(date)} className="datepicker-daygroup-item">
+                            { days.map(({ text, date, checked, isToday, inMonth, disable}, index) =>
+                                <div key={index} onClick={() => !disable && this.selectDateHandle(date)} className="datepicker-daygroup-item">
                                     <div className={
                                         classnames('datepicker-daygroup-item-text',{
+                                            disable,
                                             now: isToday,
                                             'text-muted': !inMonth,
-                                            checked: date.isSame(checkedDay)
+                                            active: date.isSame(checkedDay)
                                         })}>
                                         {text}
                                     </div>
@@ -267,17 +360,19 @@ export default class DatePicker extends Component{
                 }
                 { panel === 'month' &&
                     <div className="datepicker-body">
-                        <div className="datepicker-yeargroup">
+                        <div className="datepicker-yeargroup" ref="_yeargroup" onScroll={this.scrollYearHandle}>
                             { years.map((year, index) =>
                                 <div key={index} onClick={() => this.selectYearHandle(year)}
+                                ref={dom => (year == currentYear) && (this._activeYear=dom)}
                                 className={classnames('datepicker-yeargroup-item', {active: year == currentYear})}>
                                     {year}
                                 </div>
                             )}
                         </div>
-                        <div className="datepicker-monthgroup">
+                        <div className="datepicker-monthgroup" ref="_monthgroup">
                             { months.map((month, index) =>
                                 <div key={index} onClick={() => this.selectMonthHandle(index)}
+                                ref={dom => (index == currentMonth - 1) && (this._activeMonth=dom)}
                                 className={classnames('datepicker-monthgroup-item', {active: index == currentMonth - 1})}>
                                     {month}
                                 </div>
