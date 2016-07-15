db53e3acc16837d48d9bdaec6f17248a8f8d7280
peter <peter.shenlq@gmail.com>
2016-07-15 15:19:47 +0800

    RELEASE: 0.1.7

--- src/scripts/components/FormControlLabel.js
+++ src/scripts/components/FormControlLabel.js
@@ -1,5 +1,5 @@
 import classnames from 'classnames';
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 
 /**
  * FormControlLabel 组件.
@@ -9,6 +9,6 @@ export default class FormControlLabel extends Component{
     render(){
         let { children, className, ...others } = this.props;
 
-        return <label {...others} className={classnames('form-control-label', className)}>{children}</label>
+        return <label {...others} className={classnames('form-control-label', className)}>{children}</label>;
     }
 }
