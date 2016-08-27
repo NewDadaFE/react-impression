c7961ea38b1196657d4e1820905e09fe2f81f92b
peter <peter.shenlq@gmail.com>
2016-08-27 20:57:29 +0800

    ADD: 集成perfect-scrollbar的样式，去除transition和border-shadow判断.

--- site/src/scripts/components/impression/components/Sidebar.js
+++ site/src/scripts/components/impression/components/Sidebar.js
@@ -1,6 +1,5 @@
 import classnames from 'classnames';
 import React, { Component, PropTypes } from 'react';
-import PS from 'perfect-scrollbar';
 import Footer from './SidebarFooter';
 import Header from './SidebarHeader';
 import Body from './SidebarBody';
@@ -9,12 +8,6 @@ import Body from './SidebarBody';
  * Sidebar 组件
  */
 export default class Sidebar extends Component{
-    componentDidUpdate(){
-        PS.initialize(this.refs.container);
-    }
-    componentDidMount(){
-        PS.initialize(this.refs.container);
-    }
     //props校验
     static propTypes ={
         className: PropTypes.string,
@@ -24,7 +17,7 @@ export default class Sidebar extends Component{
         let { children, className, ...others } = this.props;
 
         return(
-            <div ref="container" {...others} className={classnames('sidebar', className)}>
+            <div {...others} className={classnames('sidebar', className)}>
                 {children}
             </div>
         );
--- src/scripts/components/SidebarBody.js
+++ src/scripts/components/SidebarBody.js
@@ -1,15 +1,23 @@
 import classnames from 'classnames';
 import React, { Component } from 'react';
+import PS from 'perfect-scrollbar';
+
 
 /**
  * Sidebar 主内容组件
  */
 export default class SidebarBody extends Component{
+    componentDidUpdate(){
+        PS.initialize(this.refs.container);
+    }
+    componentDidMount(){
+        PS.initialize(this.refs.container);
+    }
     render(){
         let { className, children, ...others } = this.props;
 
         return (
-            <div {...others} className={classnames('sidebar-body', className)}>
+            <div ref="container" {...others} className={classnames('sidebar-body', className)}>
                 { children }
             </div>
         );
