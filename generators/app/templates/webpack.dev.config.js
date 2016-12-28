a484f4123ade40d96a55a7e76495964730bb3b41
peter <peter.shenlq@gmail.com>
2016-12-28 16:35:06 +0800

    [Improve]： 集成server.js到webpack.config、整理源文件结构

--- generators/app/templates/webpack.dev.config.js
+++ generators/app/templates/webpack.dev.config.js
@@ -1,6 +1,5 @@
 var path = require('path'),
     webpack = require('webpack'),
-    HtmlWebpackPlugin = require('html-webpack-plugin'),
     DashboardPlugin = require('webpack-dashboard/plugin');
 
 
@@ -10,32 +9,39 @@ module.exports = {
         'webpack-dev-server/client?http://localhost:' + 9008,
         'webpack/hot/only-dev-server',
         'react-hot-loader/patch',
-        './src/scripts/index'
+        './src/scripts/index',
     ],
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'app.js',
-        publicPath: '/scripts/'
+        publicPath: '/scripts/',
     },
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
         new webpack.DefinePlugin({
             '__SHOW_DEV_TOOLS__': false,
             '__ENABLE_LOG__': true,
-            'NODE_ENV': 'development'
+            'NODE_ENV': 'development',
         }),
         new DashboardPlugin(),
-        // new HtmlWebpackPlugin({
-        //     filename: 'index.html',
-        //     template: './index.html',
-        //     inject: true
-        // })
     ],
     module: {
         loaders: [{
             test: /\.js$/,
             loaders: ['babel'],
-            exclude: /node_modules/
-        }]
-    }
+            exclude: /node_modules/,
+        }],
+    },
+    devServer: {
+        contentBase: './build',
+        publicPath: '/scripts',
+        hot: true,
+        historyApiFallback: true,
+        stats: {
+            colors: true,
+        },
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+        },
+    },
 };
