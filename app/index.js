58f7d28ba7f803ce1d0ff4803f0ef96a0ef6c81f
biped <poticr@gmail.com>
2017-09-16 23:04:00 +0800

    Refactor upgrade option

--- app/index.js
+++ app/index.js
@@ -1,3 +1,4 @@
+const R = require('ramda')
 const Generator = require('yeoman-generator')
 const chalk = require('chalk')
 const yosay = require('yosay')
@@ -22,18 +23,19 @@ module.exports = class extends Generator {
       )
     )
 
+    const appPackage = this.fs.readJSON(this.destinationPath('package.json'))
     const prompts = [
       {
         type: 'input',
         name: 'name',
         message: 'Your project name:',
-        default: this.appname,
+        default: this.isUpgrade ? appPackage.name : this.appname,
       },
       {
         type: 'input',
         name: 'description',
         message: 'Your project description:',
-        default: this.appname,
+        default: this.isUpgrade ? appPackage.description : this.appname,
       },
       {
         type: 'confirm',
@@ -63,31 +65,36 @@ module.exports = class extends Generator {
   writing() {
     const { name, description } = this.props
 
-    this.fs.writeJSON(
-      this.destinationPath('package.json'),
-      Object.assign(
-        {},
-        this.fs.readJSON(this.destinationPath('package.json'), {}),
-        this.fs.readJSON(this.templatePath('package.json')),
-        { name, description }
-      )
-    )
+    // Copy config files
+    this.fs.copy(this.templatePath('config/**'), this.destinationPath('config'))
 
+    // Copy example code
+    if (!this.isUpgrade) {
+      this.fs.copy(this.templatePath('src/**'), this.destinationPath('src'))
+    }
+
+    // Copy dot files
+    this.fs.copy(this.templatePath('.*'), this.destinationRoot())
+
+    // Copy package.json
+    let templatePackage = R.merge(
+      this.fs.readJSON(this.templatePath('package.json')),
+      { name, description }
+    )
+    if (this.isUpgrade) {
+      templatePackage = R.mergeDeepRight(
+        this.fs.readJSON(this.destinationPath('package.json')),
+        templatePackage
+      )
+    }
+    this.fs.writeJSON(this.destinationPath('package.json'), templatePackage)
+
+    // Copy README
     this.fs.copyTpl(
       this.templatePath('README.md'),
       this.destinationPath('README.md'),
       { name, description }
     )
-
-    this.fs.copy(
-      this.templatePath('**/!(package.json|README.md)'),
-      this.destinationRoot(),
-      { globOptions: { dot: true } }
-    )
-
-    if (this.isUpgrade) {
-      this.fs.delete(this.destinationPath('src'))
-    }
   }
 
   install() {
