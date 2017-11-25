4231824bc08a7b4db7a5990e867ed27f9257dc25
biped <poticr@gmail.com>
2017-11-25 22:42:50 +0800

    Add command of debug

--- app/templates/deploy.js
+++ app/templates/deploy.js
@@ -1,13 +1,44 @@
 const path = require('path')
+const exec = require('child_process').execSync
 const dir = require('node-dir')
 const qiniu = require('qiniu')
 
-const {
+let {
   npm_package_name: NAME,
   npm_package_version: VERSION,
   npm_package_deploy_ACCESS_KEY: ACCESS_KEY,
   npm_package_deploy_SECRET_KEY: SECRET_KEY,
 } = process.env
+
+const isDebug = process.argv.indexOf('--debug') > -1
+
+const isClean = () => {
+  const command = 'git status --porcelain'
+  const lines = exec(command)
+    .toString()
+    .trim()
+    .split('\n')
+    .filter(line => line.length)
+
+  return lines.length === 0
+}
+
+const getRevision = () => {
+  const command = 'git rev-parse --short HEAD'
+
+  return exec(command)
+    .toString()
+    .trim()
+}
+
+if (isDebug) {
+  if (isClean()) {
+    VERSION = getRevision()
+  } else {
+    console.log('请提交代码后再发测试版本！')
+    return
+  }
+}
 
 const BASE = path.resolve(__dirname, 'dist')
 const BUCKET = 'dada-fe'
