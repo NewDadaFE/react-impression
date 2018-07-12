f3f94a0ec4eea58122f2ea53edff67985b26688a
biped <poticr@gmail.com>
2018-07-13 00:19:48 +0800

    Update config

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,14 +1,36 @@
+const path = require('path')
+
 module.exports = {
   title: 'React Impression',
   serverPort: 8080,
+  require: [path.join(__dirname, 'src/styles/index.scss')],
   components: 'src/components/**/[A-Z]*.js',
+  showUsage: true,
+  getComponentPathLine(componentPath) {
+    const name = path.basename(componentPath, '.js')
+
+    return `import { ${name} } from 'react-impression'`
+  },
   webpackConfig: {
     module: {
       rules: [
         {
-          test: /\.jsx?$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
+          oneOf: [
+            {
+              test: /\.(png|jpe?g|gif|svg)$/,
+              loader: 'url-loader',
+            },
+            {
+              test: /\.jsx?$/,
+              exclude: /node_modules/,
+              loader: 'babel-loader',
+            },
+            {
+              test: /\.s?css$/,
+              exclude: /node_modules/,
+              use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+          ],
         },
       ],
     },
