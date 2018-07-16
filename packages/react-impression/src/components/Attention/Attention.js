98584536c23cb09d4cd336d4a87f6bc2fb4d0710
wj <wangjin@imdada.cn>
2018-07-16 18:29:22 +0800

    [ADD] attention components

--- packages/react-impression/src/components/Attention/Attention.js
+++ packages/react-impression/src/components/Attention/Attention.js
@@ -1,40 +1,48 @@
 import classnames from 'classnames'
-import React, { PureComponent } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import AttentionLink from '../AttentionLink'
 
-/**
- * Attention 组件
- */
-export default class Attention extends PureComponent {
-  // 初始state
+export default class Attention extends React.PureComponent {
   constructor(props, context) {
     super(props, context)
     this.state = {
       show: true,
     }
   }
-  // props 校验
+
   static propTypes = {
-    // 样式（success、primary、warning、danger）
-    theme: PropTypes.string,
-    // 是否可关闭
+    /**
+     * 主题（success、primary、warning、danger）
+     */
+    theme: PropTypes.oneOf(['warning', 'success', 'primary', 'danger']),
+
+    /**
+     * 是否可关闭
+     */
     closeable: PropTypes.bool,
-    // 自定义样式
+
+    /**
+     * 自定义样式
+     */
     className: PropTypes.string,
+
+    /**
+     * 子元素
+     */
     children: PropTypes.any,
   }
+
   // 关闭
   hideHandle = () => {
     this.setState({
       show: false,
     })
   }
-  // 渲染
+
   render() {
-    let { theme, className, closeable, children, ...others } = this.props,
-      themeClass = `attention-${theme}`,
-      hiddenClass = this.state.show ? '' : 'hidden'
+    const { theme, className, closeable, children, ...others } = this.props
+    const themeClass = `attention-${theme}`
+    const hiddenClass = this.state.show ? '' : 'hidden'
 
     return (
       <div
@@ -51,5 +59,3 @@ export default class Attention extends PureComponent {
     )
   }
 }
-
-Attention.Link = AttentionLink
