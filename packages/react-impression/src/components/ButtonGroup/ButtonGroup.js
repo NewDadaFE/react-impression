1c813bc33edea23b8e4ccf54dbcddae10581a3ce
wjeek <1248997541@qq.com>
2018-07-17 15:16:48 +0800

    [ADD] ButtonGroup ButtonToolbar

--- packages/react-impression/src/components/ButtonGroup/ButtonGroup.js
+++ packages/react-impression/src/components/ButtonGroup/ButtonGroup.js
@@ -1,47 +1,62 @@
 import classnames from 'classnames'
-import React, { PureComponent } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-/**
- * ButtonGroup组件.
- */
-export default class ButtonGroup extends PureComponent {
-  // prop校验
+
+export default class ButtonGroup extends React.PureComponent {
   static propTypes = {
-    // 大小（lg、sm）
+    /**
+     * 大小（lg、sm或者默认）
+     */
     size: PropTypes.string,
-    // 主题样式(primary、secondary、default)
-    theme: PropTypes.string,
-    // 激活索引（被选中Button会额外添加选中样式，为空时不额外添加选中样式）
+
+    /**
+     * 主题样式
+     */
+    theme: PropTypes.oneOf(['primary', 'secondary', 'default']),
+
+    /**
+     * 激活索引（被选中Button会额外添加选中样式，为空时不额外添加选中样式）
+     */
     activeKey: PropTypes.any,
-    // 选中回调
+
+    /**
+     * 选中回调
+     */
     onSelect: PropTypes.func,
-    // 自定义样式
+
+    /**
+     * 自定义样式
+     */
     className: PropTypes.string,
+
+    /**
+     * 子组件
+     */
     children: PropTypes.any,
   }
-  // 默认props
+
   static defaultProps = {
     theme: 'default',
   }
-  // 初始state
+
   constructor(props, context) {
     super(props, context)
     this.state = {
       activeKey: props.activeKey,
     }
   }
-  // 渲染
+
   render() {
-    let { activeKey } = this.state,
-      { theme, size, className, onSelect, children, ...others } = this.props,
-      btnGroupSize = size ? `btn-group-${size}` : null
+    let { theme, size, className, onSelect, children, ...others } = this.props
+    const { activeKey } = this.state
+    const btnGroupSize = size ? `btn-group-${size}` : null
 
     delete others.activeKey
     children = children.map((child, idx) => {
       let { eventKey, onClick } = child.props
 
       return React.cloneElement(child, {
-        key: idx, // eslint-disable-line
+        key: idx,
         outline: theme !== 'default' && (!onSelect || activeKey !== eventKey),
         theme:
           theme === 'default' && onSelect && activeKey === eventKey
