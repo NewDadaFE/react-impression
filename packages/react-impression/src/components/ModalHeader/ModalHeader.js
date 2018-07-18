a6ae76457a2ed767dfb3aabb8179d9e6f2d96d5d
lusssn <lusssn@126.com>
2018-07-18 10:48:40 +0800

    U 添加示例，修改注释

--- packages/react-impression/src/components/ModalHeader/ModalHeader.js
+++ packages/react-impression/src/components/ModalHeader/ModalHeader.js
@@ -1,9 +1,7 @@
 import classnames from 'classnames'
 import React from 'react'
 import PropTypes from 'prop-types'
-/**
- * ModalHeader组件.
- */
+
 const ModalHeader = ({ className, children, ...others }) => {
   return (
     <div {...others} className={classnames('modal-header', className)}>
@@ -13,7 +11,13 @@ const ModalHeader = ({ className, children, ...others }) => {
 }
 
 ModalHeader.propTypes = {
+  /**
+   * 子组件
+   */
   children: PropTypes.any,
+  /**
+   * 自定义样式
+   */
   className: PropTypes.string,
 }
 
