5a971935fe86aa3a8504a4fa2cfcf941811b821c
wjeek <1248997541@qq.com>
2018-08-09 16:14:57 +0800

    Modal enhance scroll (#85)
    * [fix] Modal组件内容过长问题 #76
    
    * [fix] Modal组件内容过长问题
    
    * [fix] Modal proptypes
    
    * [fix] Modal scrollInside
    
    * [fix] Modal scrollInside annotation
    
    * [fix] Modal scrollInside example
    
    * [add] changlog
    
    * [add] changlog
    
    * [fix] modal api changelog

--- packages/react-impression/src/components/Modal/Modal.js
+++ packages/react-impression/src/components/Modal/Modal.js
@@ -5,32 +5,77 @@ import ModalHeader from '../ModalHeader'
 import ModalBody from '../ModalBody'
 import ModalFooter from '../ModalFooter'
 
-const Modal = ({ size, className, children, ...others }) => {
-  const sizeClass = size ? `modal-${size}` : null
+class Modal extends React.Component {
+  static propTypes = {
+    /**
+     * 子组件
+     */
+    children: PropTypes.node,
 
-  return (
-    <div {...others} className={classnames('modal', className)}>
-      <div className={classnames('modal-dialog', sizeClass)}>
-        <div className='modal-content slideInDown'>{children}</div>
+    /**
+     * 自定义样式
+     */
+    className: PropTypes.string,
+
+    /**
+     * 大小
+     */
+    size: PropTypes.oneOf(['sm', 'lg']),
+
+    /**
+     * Modal是否内部滚动
+     */
+    scrollInside: PropTypes.bool,
+  }
+
+  static defaultProps = {
+    scrollInside: false,
+  }
+
+  componentDidMount() {
+    this.disableScroll()
+  }
+
+  componentWillUnmount() {
+    this.enableScroll()
+  }
+
+  disableScroll() {
+    const documentBody = document.body
+
+    if (documentBody) {
+      documentBody.style.setProperty('overflow', 'hidden')
+    }
+  }
+
+  enableScroll() {
+    const documentBody = document.body
+
+    if (documentBody) {
+      documentBody.style.removeProperty('overflow')
+    }
+  }
+
+  render() {
+    const {
+      size,
+      className,
+      children,
+      scrollInside,
+      ...others
+    } = this.props
+    const sizeClass = size ? `modal-${size}` : null
+
+    return (
+      <div {...others} className={classnames('modal', { 'limit-height': scrollInside }, className)}>
+        <div className={classnames('modal-dialog', sizeClass)}>
+          <div className='modal-content slideInDown'>{children}</div>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
-Modal.propTypes = {
-  /**
-   * 子组件
-   */
-  children: PropTypes.node,
-  /**
-   * 自定义样式
-   */
-  className: PropTypes.string,
-  /**
-   * 大小
-   */
-  size: PropTypes.oneOf(['sm', 'lg']),
-}
 Modal.Header = ModalHeader
 Modal.Body = ModalBody
 Modal.Footer = ModalFooter
