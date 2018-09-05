4858eb9dea25b7f83f322224b0fa652fec2cf817
wj <wangjin@imdada.cn>
2018-09-05 21:38:54 +0800

    add portal

--- packages/react-impression/src/components/Modal/Modal.js
+++ packages/react-impression/src/components/Modal/Modal.js
@@ -1,5 +1,6 @@
 import classnames from 'classnames'
 import React from 'react'
+import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import ModalHeader from '../ModalHeader'
 import ModalBody from '../ModalBody'
@@ -8,6 +9,8 @@ import ModalFooter from '../ModalFooter'
 const KeyCode = {
   ESC: 27,
 }
+
+const IS_REACT_16 = 'createPortal' in ReactDOM
 
 class Modal extends React.Component {
   static propTypes = {
@@ -46,16 +49,31 @@ class Modal extends React.Component {
     scrollInside: false,
   }
 
+  /**
+   * modal组件的ref
+   */
+  wrap = null
+
   getWrap = ref => {
     this.wrap = ref
+    if (ref) {
+      this.wrap.focus()
+    }
   }
 
+  /**
+   * 如果是react16则在body下创建div，并触发render更新
+   */
   componentDidMount() {
+    IS_REACT_16 && this.createContainer()
     this.disableScroll()
-    this.wrap.focus()
   }
 
+  /**
+   * 如果是react16则删除渲染的modal的dom节点
+   */
   componentWillUnmount() {
+    IS_REACT_16 && this.removeContainer()
     this.enableScroll()
   }
 
@@ -67,18 +85,28 @@ class Modal extends React.Component {
   }
 
   disableScroll() {
-    const documentBody = document.body
-
-    if (documentBody) {
-      documentBody.style.setProperty('overflow', 'hidden')
-    }
+    document.body.style.setProperty('overflow', 'hidden')
   }
 
   enableScroll() {
-    const documentBody = document.body
+    document.body.style.removeProperty('overflow')
+  }
 
-    if (documentBody) {
-      documentBody.style.removeProperty('overflow')
+  createContainer() {
+    this._container = this.getContainer()
+    this.forceUpdate()
+  }
+
+  getContainer = () => {
+    const container = document.createElement('div')
+
+    document.body.appendChild(container)
+    return container
+  }
+
+  removeContainer() {
+    if (this._container) {
+      this._container.parentNode.removeChild(this._container)
     }
   }
 
@@ -86,7 +114,10 @@ class Modal extends React.Component {
     const { size, className, children, scrollInside, ...others } = this.props
     const sizeClass = size ? `modal-${size}` : null
 
-    return (
+    delete others.onClose
+    delete others.keyboard
+
+    const portalChildren = (
       <div
         {...others}
         ref={this.getWrap}
@@ -103,6 +134,16 @@ class Modal extends React.Component {
         </div>
       </div>
     )
+
+    if (IS_REACT_16) {
+      if (this._container) {
+        return ReactDOM.createPortal(portalChildren, this._container)
+      } else {
+        return null
+      }
+    }
+
+    return portalChildren
   }
 }
 
