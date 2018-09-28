7a5d116694236647ffa2fba1e35eaaaef158ba64
wjeek <1248997541@qq.com>
2018-09-28 19:34:45 +0800

    fix confirm button (#256)

--- packages/react-impression/src/components/Confirm/Confirm.js
+++ packages/react-impression/src/components/Confirm/Confirm.js
@@ -1,6 +1,7 @@
 import classnames from 'classnames'
 import React from 'react'
 import PropTypes from 'prop-types'
+import Button from '../Button'
 
 export default class Confirm extends React.PureComponent {
   static propTypes = {
