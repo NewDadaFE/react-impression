c4e1955e5ff5f3fcc3c61851f4b1828c7469f7d3
biped <poticr@gmail.com>
2018-09-29 16:13:31 +0800

    Integrate website and examples (#266)
    * Downgrade to webpack 3
    
    * Clean up
    
    * Update babel config
    
    * Build and copy website and examples
    
    * Bypass process of yarn installation
    
    * Add basename
    
    * Fix build
    
    * Enable tree-shaking and fix relative path
    
    * Revert "Downgrade to webpack 3"
    
    This reverts commit 7676ff257cf3f00adc44012be9488e5f8b563241.
    
    * Add link to examples
    
    * Fix dir
    
    * Fix bug of build
    
    * Fix case problem
    
    * Fix case problem
    
    * Fix dist

---
+++ scripts/build.js
@@ -0,0 +1,18 @@
+/**
+ * Build
+ */
+const { execSync } = require('child_process')
+const shell = command => execSync(command, { stdio: 'inherit' })
+
+shell('yarn workspace react-impression build')
+shell('yarn workspace react-impression-website build')
+shell('yarn workspace react-impression-examples build')
+
+/**
+ * Copy
+ */
+const fs = require('fs-extra')
+
+fs.emptyDirSync('dist')
+fs.copySync('packages/react-impression-website/dist', 'dist')
+fs.copySync('packages/react-impression-examples/dist', 'dist/examples')
