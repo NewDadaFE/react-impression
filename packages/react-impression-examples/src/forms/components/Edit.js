3daf7ec0dc78cb3c2477b493f2ac93a04168bc61
tanghongyan <tanghongyan@imdada.cn>
2018-12-13 11:06:59 +0800

    fix

--- packages/react-impression-examples/src/forms/components/Edit.js
+++ packages/react-impression-examples/src/forms/components/Edit.js
@@ -1,7 +1,40 @@
 import React from 'react'
+import { Row, Col, Card, Select, SelectOption, Input } from 'react-impression'
+import styles from './Edit.scss'
 
 function Edit() {
-  return <div>222</div>
+  return (
+    <Card block>
+      <Row>
+        <Col>
+          城市：
+          <Select>
+            <SelectOption value='1'>哈哈</SelectOption>
+          </Select>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          拒绝次数：
+          <Input className={styles.side} type='text' />次
+        </Col>
+        <Col>
+          扣款金额：
+          <Input type='text' />元
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          每单补贴：
+          <Input type='text' />元
+        </Col>
+        <Col>
+          听单时长：
+          <Input type='text' />分钟
+        </Col>
+      </Row>
+    </Card>
+  )
 }
 
 export default Edit
--- packages/react-impression-examples/src/forms/container.js
+++ packages/react-impression-examples/src/forms/container.js
@@ -3,11 +3,14 @@ import {
   Nav,
   NavItem,
   Card,
-  Form,
-  FormGroup,
-  Input,
-  Button,
+  // Form,
+  // FormGroup,
+  // Input,
+  // Button,
+  // Row,
+  // Col,
 } from 'react-impression'
+import Edit from './components/Edit'
 import './container.module.scss'
 
 class FormInfo extends Component {
@@ -23,36 +26,7 @@ class FormInfo extends Component {
             <NavItem eventKey={1}>城市规则</NavItem>
             <NavItem eventKey={2}>派单规则</NavItem>
           </Nav>
-          <div>
-            <Form>
-              <FormGroup>
-                <label>姓名</label>
-                <Input type='text' placeholder='Jane Doe' />
-              </FormGroup>
-              <FormGroup>
-                <label>邮箱</label>
-                <Input type='text' placeholder='hello@example.com' />
-              </FormGroup>
-              <FormGroup>
-                <label>电话</label>
-                <Input type='text' placeholder='15698801833' />
-              </FormGroup>
-              <FormGroup>
-                <label>联系人</label>
-                <Input type='text' placeholder='填写联系人' />
-              </FormGroup>
-              <FormGroup>
-                <label>爱好</label>
-                <Input type='text' placeholder='编程' />
-              </FormGroup>
-              <FormGroup>
-                <Button theme='secondary'>搜索</Button>
-                <Button theme='default' style={{ marginLeft: 20 }}>
-                  重置
-                </Button>
-              </FormGroup>
-            </Form>
-          </div>
+          <Edit />
         </Card>
       </div>
     )
