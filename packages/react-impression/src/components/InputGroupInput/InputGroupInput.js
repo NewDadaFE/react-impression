3cb075477b70ae3411a5789b5f063b4abed1c67c
sdshaoda <21106848+sdshaoda@users.noreply.github.com>
2022-03-01 14:26:33 +0800

    优化 InputGroupInput (#677)
    Co-authored-by: shaoda <shaoda@imdada.cn>

--- packages/react-impression/src/components/InputGroupInput/InputGroupInput.js
+++ packages/react-impression/src/components/InputGroupInput/InputGroupInput.js
@@ -31,13 +31,17 @@ export default class InputGroupInput extends React.PureComponent {
   }
 
   render() {
-    const { type, className, placeholder, ...others } = this.props
+    const { type, className, style, placeholder, ...others } = this.props
 
     return (
-      <div {...others} className='dada-input-group-input'>
+      <div
+        className={classnames('dada-input-group-input', className)}
+        style={style}
+      >
         <input
+          {...others}
           type={type}
-          className={classnames('form-control', 'input-field', className)}
+          className='form-control input-field'
           placeholder={placeholder}
           ref={ref => (this.refMain = ref)}
         />
